8737eed23e619a059f49801abc21ee312fda7986
kishanaik5 <kishanknaik03@gmail.com>
2026-08-28 22:23:48 +0530

    fix(cloudflare): export static HTML to ./out and configure wrangler to serve ./out directly

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,10 @@ import type { NextConfig } from "next";
 const isGithubActions = process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
-  output: isGithubActions ? "export" : undefined,
+  output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isGithubActions ? "/narviya_website_test" : undefined,
 };
 
 export default nextConfig;
